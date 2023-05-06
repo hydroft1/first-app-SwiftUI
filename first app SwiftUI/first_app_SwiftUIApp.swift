2be353def5d86160757ec4cc8eea28cab7cd75f8
hydroft1 <hydroftcount@hotmail.com>
2023-05-06 23:35:19 +0200

    Initial Commit

---
+++ first app SwiftUI/first_app_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_app_SwiftUIApp.swift
+//  first app SwiftUI
+//
+//  Created by Alexandre Marquet on 06/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct first_app_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
